029288f5a96af66ca64e91db2bc810bbabf1bac6
petri <petri@LAPTOP-U9BND4HO>
2024-09-08 18:10:28 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVXFirstApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
